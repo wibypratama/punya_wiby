beabcb90eacebc15c33e2663e9fc513d8037d297
wibypratama <wibypratamam@gmail.com>
2026-05-07 16:54:59 +0700

    Update

--- t_produk.php
+++ t_produk.php
@@ -282,7 +282,7 @@ if (isset($_POST['simpan'])) {
     <!-- ======= Footer ======= -->
     <footer id="footer" class="footer">
         <div class="copyright">
-            &copy; Copyright <strong><span>WIBY</span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>Nama Sistem</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
             Designed by <a href="">WIBY</a>
