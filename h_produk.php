a86d2d4013f2546f6b38b5ab404b6fdea053cbfd
wibypratama <wibypratamam@gmail.com>
2026-05-14 19:56:07 +0700

    Membuat File h_produk

--- h_produk.php
+++ h_produk.php
@@ -3,6 +3,17 @@ include "Koneksi.php";
 
 $id = $_GET['id'];
 
+// ambil nama file gambar
+$get = mysqli_query($conn, "SELECT gambar FROM products WHERE id='$id'");
+$data = mysqli_fetch_array($get);
+$gambar = $data['gambar'];
+
+// hapus file gambar jika ada
+if ($gambar != "" && file_exists("produk_img/" . $gambar)) {
+    unlink("produk_img/" . $gambar);
+}
+
+//hapus data dari database
 $hapus = mysqli_query($conn, "DELETE FROM products WHERE id='$id'");
 
 if ($hapus) {
